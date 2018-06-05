feef89c6277a8438e0296fe3ff5f1026194dd748
destroyer <joel250598@gmail.com>
2018-06-05 16:25:24 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal();
+		sr.reveal('.navbar', {
+			duration:2000,
+			origin: 'botom'
+		});
+		sr.reveal('.header-content-left', {
+			duration:3500,
+			origin: 'left'
+			
+		});
+		sr.reveal('.header-content-right', {
+			duration:3500,
+			origin: 'right'
+			
+		});
+
